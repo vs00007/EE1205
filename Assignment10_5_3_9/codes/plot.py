47ca708f5d741ae8e27b3d08d2e60022a4425049
vs00007 <siddharthavemula07@gmail.com>
2024-01-08 16:35:26 +0530

    Updated files

---
+++ Assignment10_5_3_9/codes/plot.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def x(n):
+        return n**2
+n_values= np.arange(-3, 10, 1)
+
+y= np.heaviside(n_values, 0) *x(n_values)
+
+plt.stem(n_values, y, linefmt='-', markerfmt='o', basefmt='r', label=r'$n^2$')
+
+plt.xlabel('n')
+plt.ylabel("x(n)")
+plt.title("Stem Plot of $x(n)$")
+plt.legend()
+plt.grid(True)
+plt.show()
